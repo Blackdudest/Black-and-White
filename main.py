5d04ed8da820c07d83af940f5d5e9a6fa5b62eac
Blackdudest <adeoyesamson24@gmail.com>
2022-12-16 00:57:42 +0100

    Corrected the code by prompting the code when there is an invalid message

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ name = input("What is your name? ")
 if 3 < len(name) < 11:
   print("Welcome " + name)
 
-else:
-    print(name + " is invalid! Try characters between 3 and 11")
+  from PIL import Image
+  file = r"C:\Users\adeoy\OneDrive\Desktop\pexels-cindy-gustafson-675313.jpg"
+  img = Image.open(file)
+  img = img.convert("L")
+  img.show()
 
-from PIL import Image
-file = r"C:\Users\adeoy\OneDrive\Desktop\pexels-cindy-gustafson-675313.jpg"
-img = Image.open(file)
-img = img.convert("L")
-img.show()
+else:
+    print(name + " is invalid! Try characters from 3-11 letters")
+
